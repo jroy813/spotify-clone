f2114c28992efdcb207d26aad8f810b61bf20dbf
DESKTOP-HB65II7\Justin <jroy@sociusmarketing.com>
2020-08-24 21:03:03 -0400

    Adding sidebar options

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,14 +2,23 @@ import React from 'react'
 import '../css/Sidebar.css'
 import SpotifyLogo from '../images/spotify-logo.png'
 import SidebarOption from './SidebarOption'
+import HomeIcon from '@material-ui/icons/Home'
+import SearchIcon from '@material-ui/icons/Search'
+import LibraryMusician from '@material-ui/icons/LibraryMusic'
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img src={SpotifyLogo} className="sidebar__logo" alt=""/>
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusician} title="Your Library" />
+            
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+            
+            <SidebarOption title="Hip Hop" />
         </div>
     )
 }
